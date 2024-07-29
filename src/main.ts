a28edc773db1742db3312bf57d4b71b29bda5892
37o1 <theneonman0@gmail.com>
2024-07-29 12:26:12 +0200

    delete test

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
 import { randomUUID } from "crypto";
 import { FireballRegExp, genFireball } from "./fireball";
 import { createWriteStream } from "fs";
-
-const stream = createWriteStream("randfire.txt");
-
-const l = () => stream.write(genFireball() + "\n");
-
-let i = 0;
-
-setInterval(() => {
-	i++;
-
-	if (i >= 50000) {
-		stream.close();
-		throw 0;
-	};
-
-	l();
-});
