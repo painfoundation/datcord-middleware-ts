31c52a328f59aa8dd9ba302df8e21e6d0569dfcd
37o1 <theneonman0@gmail.com>
2024-07-29 14:50:05 +0200

    add default

--- src/fireball.ts
+++ src/fireball.ts
@@ -1,6 +1,6 @@
 import { randomUUID, UUID } from "crypto";
 
-
 export type Fireball = `${UUID}/${UUID}/${UUID}/${UUID}`;
 export const FireballRegExp = /^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})(\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}){3}$/;
 export const genFireball = () => randomUUID()+"/"+randomUUID()+"/"+randomUUID()+"/"+randomUUID() as Fireball;
+export const defaultFireball: Fireball = "00000000-0000-4000-8000-000000000000/00000000-0000-4000-8000-000000000000/00000000-0000-4000-8000-000000000000/00000000-0000-4000-8000-000000000000";
